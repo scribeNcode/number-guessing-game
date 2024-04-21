46c66dd09255d61c6eb88f6ef2c7973316a7c923
scribeNcode <deyeye99@gmail.com>
2024-04-21 22:36:25 +0100

    add game functionalities

--- index.js
+++ index.js
@@ -9,18 +9,64 @@ let sarcasticIndex = Math.floor((Math.random() * (5 - 0))) + 0
 let sarcasticMessage = sarcasticInput[sarcasticIndex]
 console.log(sarcasticMessage)
 
-//Take userInput
-let inPutTag = document.querySelector('.userGuessNumber')
-let submitButton = document.querySelector('.submitBut')
-let displayDiv = document.querySelector('submitButResult')
 
-submitButton.addEventListener('click',(e)=>{
+let userInputNum = document.querySelector('.userGuessNumberInput')
+//GET DISPALY DIV
+let displayDiv = document.querySelector('.submitButResult')
+//Target submit button 
+let button = document.querySelector('.submitBut');
+// Get form tag
+let gameSec = document.querySelector('.gameSection')
+
+//Numbers of times button is clicked
+let numOfClick = 0
+button.addEventListener('click', (e)=>{
     e.preventDefault()
-    let userTypedNum = document.createElement('p')
-    userTypedNum.classList.add('userTypedP')
-    userTypedNum.innerText = inPutTag.value
-    displayDiv.appendChild(userTypedNum)
+    //create a p tag and add a class
+  let pTag =  document.createElement('p');
+  pTag.classList.add('userNum')
+
+  //get num of times button is clicked
+  numOfClick++
+  let numbersOfAttemptLeft = 3 - numOfClick
+  attemptsTag = document.querySelector('.numOfAttempts')
+  attemptsTag.innerText = numbersOfAttemptLeft
+  console.log(numbersOfAttemptLeft)
+  console.log(numOfClick)
+  console.log(userInputNum)
+  console.log(randomNum)
+
+  if(displayDiv.childNodes.length > 0){
+    displayDiv.innerHTML = ''
+}
+
+if(numOfClick === 3){
+    button.disabled=true
+    button.style.backgroundColor = "#e5e5e5"
+    pTag.innerText = "You have reached the maximum number of trial"
+    //create funny text tag
+    let funnyReply = document.createElement('p')
+    funnyReply.classList.add('funnyReplyStyle')
+    funnyReply.innerText = `${sarcasticMessage}, The correct number is ${randomNum}`
+    gameSec.appendChild(funnyReply)
+}
+
+  if(userInputNum.value === randomNum ){
+    pTag.innerText = "CONGRATULATIONS!"
+}else if(userInputNum.value < randomNum ){
+    pTag.innerText = "It is lesser than the number"
+}else if(userInputNum.value > randomNum){
+    pTag.innerText = "It is greater than the number"
+}else{
+    pTag.innerText ="End"
+}
+
+//   pTag.innerText = userInputNum.value
+  displayDiv.appendChild(pTag)
+    
 })
+
+
 
 
 

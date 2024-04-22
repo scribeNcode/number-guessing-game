2f5a292bc8c4adcc20685db143115ac929f94a66
scribeNcode <deyeye99@gmail.com>
2024-04-22 23:07:43 +0100

    add more functionality

--- index.js
+++ index.js
@@ -3,23 +3,29 @@
 let randomNum = Math.floor(Math.random() * (100 - 0 )) + 1
 console.log(randomNum)
 
-//An array of sarcastic message with 5 0ptions
-let sarcasticInput = ["hubbyy", "woo", "intoo", "olodo", "shame"]
+//An array of sarcastic message with 5 possible 0ptions
+let sarcasticInput = ["you've just scored a golden ticket to the ultimate comedy of errors!", "you're now enrolled in the School of Shenanigans and you're top of the class!", "you've unlocked the gateway to the land of laughter and lunacy!", "Congratulations, you're officially the mayor of the town called 'Hilarity Ensues'!", "Congratulations, you're now a card-carrying member of the Society of Silly Surprises!"]
 let sarcasticIndex = Math.floor((Math.random() * (5 - 0))) + 0
 let sarcasticMessage = sarcasticInput[sarcasticIndex]
 console.log(sarcasticMessage)
 
+// Getting elements 
 
 let userInputNum = document.querySelector('.userGuessNumberInput')
 //GET DISPALY DIV
 let displayDiv = document.querySelector('.submitButResult')
 //Target submit button 
 let button = document.querySelector('.submitBut');
-// Get form tag
+// Get game section tag
 let gameSec = document.querySelector('.gameSection')
+//Get reload button
+let reloadButton = document.querySelector('.reloadPage')
+
 
 //Numbers of times button is clicked
 let numOfClick = 0
+
+
 button.addEventListener('click', (e)=>{
     e.preventDefault()
     //create a p tag and add a class
@@ -30,7 +36,7 @@ button.addEventListener('click', (e)=>{
   numOfClick++
   let numbersOfAttemptLeft = 3 - numOfClick
   attemptsTag = document.querySelector('.numOfAttempts')
-  attemptsTag.innerText = numbersOfAttemptLeft
+  attemptsTag.innerText = `Number of attempts left : ${numbersOfAttemptLeft}`
   console.log(numbersOfAttemptLeft)
   console.log(numOfClick)
   console.log(userInputNum)
@@ -41,25 +47,43 @@ button.addEventListener('click', (e)=>{
 }
 
 if(numOfClick === 3){
+  
     button.disabled=true
-    button.style.backgroundColor = "#e5e5e5"
+    button.style.backgroundColor = "grey"
+    button.style.color ="white"
+    button.style.weight = 'bolder'
+    button.style.display = "none"
+    reloadButton.style.display = "block"
     pTag.innerText = "You have reached the maximum number of trial"
     //create funny text tag
     let funnyReply = document.createElement('p')
     funnyReply.classList.add('funnyReplyStyle')
-    funnyReply.innerText = `${sarcasticMessage}, The correct number is ${randomNum}`
+
+    // gameSec.appendChild(funnyReply)
+    funnyReply.innerHTML = `<p>${sarcasticMessage} &#128514;, The correct number is  <span class="emphasy">${randomNum}</span> </p>`
     gameSec.appendChild(funnyReply)
 }
 
+
+// Logic
   if(userInputNum.value === randomNum ){
-    pTag.innerText = "CONGRATULATIONS!"
+    pTag.innerText = "CONGRATULATIONS! You guessed right"
 }else if(userInputNum.value < randomNum ){
-    pTag.innerText = "It is lesser than the number"
+    pTag.innerText = "Sorry your input is lesser than the generated random number.  Try again!"
 }else if(userInputNum.value > randomNum){
-    pTag.innerText = "It is greater than the number"
+    pTag.innerText = "Sorry your input is greater than the generated randomnumber.  Try again!"
 }else{
     pTag.innerText ="End"
 }
+
+
+// Reload the current page
+reloadButton.addEventListener('click', (e)=>{
+  e.preventDefault()
+  window.location.reload();
+})
+
+
 
 //   pTag.innerText = userInputNum.value
   displayDiv.appendChild(pTag)
@@ -69,7 +93,3 @@ if(numOfClick === 3){
 
 
 
-
-
-
-// luke 21 / 12 vrs 5
